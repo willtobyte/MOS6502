c3d3ee071a4e957c3300eaed42886b9570c76ed6
Rodrigo Delduca <rodrigo@delduca.org>
2025-02-16 11:07:22 -0300

    Work in progress

--- 6502.lua
+++ 6502.lua
@@ -1,369 +1,369 @@
 local opcodes = {
   -- ADC (Add with Carry)
   -- ADC Immediate
-  [0x69] = function() end,
+  [0x69] = { cycles = 2, exec = function(cpu) end },
   -- ADC Zero Page
-  [0x65] = function() end,
+  [0x65] = { cycles = 3, exec = function(cpu) end },
   -- ADC Zero Page,X
-  [0x75] = function() end,
+  [0x75] = { cycles = 4, exec = function(cpu) end },
   -- ADC Absolute
-  [0x6D] = function() end,
+  [0x6D] = { cycles = 4, exec = function(cpu) end },
   -- ADC Absolute,X
-  [0x7D] = function() end,
+  [0x7D] = { cycles = 4, exec = function(cpu) end },
   -- ADC Absolute,Y
-  [0x79] = function() end,
+  [0x79] = { cycles = 4, exec = function(cpu) end },
   -- ADC (Indirect,X)
-  [0x61] = function() end,
+  [0x61] = { cycles = 6, exec = function(cpu) end },
   -- ADC (Indirect),Y
-  [0x71] = function() end,
+  [0x71] = { cycles = 5, exec = function(cpu) end },
 
   -- AND (Logical AND)
   -- AND Immediate
-  [0x29] = function() end,
+  [0x29] = { cycles = 2, exec = function(cpu) end },
   -- AND Zero Page
-  [0x25] = function() end,
+  [0x25] = { cycles = 3, exec = function(cpu) end },
   -- AND Zero Page,X
-  [0x35] = function() end,
+  [0x35] = { cycles = 4, exec = function(cpu) end },
   -- AND Absolute
-  [0x2D] = function() end,
+  [0x2D] = { cycles = 4, exec = function(cpu) end },
   -- AND Absolute,X
-  [0x3D] = function() end,
+  [0x3D] = { cycles = 4, exec = function(cpu) end },
   -- AND Absolute,Y
-  [0x39] = function() end,
+  [0x39] = { cycles = 4, exec = function(cpu) end },
   -- AND (Indirect,X)
-  [0x21] = function() end,
+  [0x21] = { cycles = 6, exec = function(cpu) end },
   -- AND (Indirect),Y
-  [0x31] = function() end,
+  [0x31] = { cycles = 5, exec = function(cpu) end },
 
   -- ASL (Arithmetic Shift Left)
   -- ASL Accumulator
-  [0x0A] = function() end,
+  [0x0A] = { cycles = 2, exec = function(cpu) end },
   -- ASL Zero Page
-  [0x06] = function() end,
+  [0x06] = { cycles = 5, exec = function(cpu) end },
   -- ASL Zero Page,X
-  [0x16] = function() end,
+  [0x16] = { cycles = 6, exec = function(cpu) end },
   -- ASL Absolute
-  [0x0E] = function() end,
+  [0x0E] = { cycles = 6, exec = function(cpu) end },
   -- ASL Absolute,X
-  [0x1E] = function() end,
+  [0x1E] = { cycles = 7, exec = function(cpu) end },
 
   -- Branch Instructions
   -- BCC Relative
-  [0x90] = function() end,
+  [0x90] = { cycles = 2, exec = function(cpu) end },
   -- BCS Relative
-  [0xB0] = function() end,
+  [0xB0] = { cycles = 2, exec = function(cpu) end },
   -- BEQ Relative
-  [0xF0] = function() end,
+  [0xF0] = { cycles = 2, exec = function(cpu) end },
   -- BMI Relative
-  [0x30] = function() end,
+  [0x30] = { cycles = 2, exec = function(cpu) end },
   -- BNE Relative
-  [0xD0] = function() end,
+  [0xD0] = { cycles = 2, exec = function(cpu) end },
   -- BPL Relative
-  [0x10] = function() end,
+  [0x10] = { cycles = 2, exec = function(cpu) end },
   -- BVC Relative
-  [0x50] = function() end,
+  [0x50] = { cycles = 2, exec = function(cpu) end },
   -- BVS Relative
-  [0x70] = function() end,
+  [0x70] = { cycles = 2, exec = function(cpu) end },
 
   -- BIT (Bit Test)
   -- BIT Zero Page
-  [0x24] = function() end,
+  [0x24] = { cycles = 3, exec = function(cpu) end },
   -- BIT Absolute
-  [0x2C] = function() end,
+  [0x2C] = { cycles = 4, exec = function(cpu) end },
 
   -- BRK (Force Interrupt)
   -- BRK Implied
-  [0x00] = function() end,
+  [0x00] = { cycles = 7, exec = function(cpu) end },
 
   -- Flag Instructions
   -- CLC Implied
-  [0x18] = function() end,
+  [0x18] = { cycles = 2, exec = function(cpu) end },
   -- CLD Implied
-  [0xD8] = function() end,
+  [0xD8] = { cycles = 2, exec = function(cpu) end },
   -- CLI Implied
-  [0x58] = function() end,
+  [0x58] = { cycles = 2, exec = function(cpu) end },
   -- CLV Implied
-  [0xB8] = function() end,
+  [0xB8] = { cycles = 2, exec = function(cpu) end },
 
   -- CMP (Compare Accumulator)
   -- CMP Immediate
-  [0xC9] = function() end,
+  [0xC9] = { cycles = 2, exec = function(cpu) end },
   -- CMP Zero Page
-  [0xC5] = function() end,
+  [0xC5] = { cycles = 3, exec = function(cpu) end },
   -- CMP Zero Page,X
-  [0xD5] = function() end,
+  [0xD5] = { cycles = 4, exec = function(cpu) end },
   -- CMP Absolute
-  [0xCD] = function() end,
+  [0xCD] = { cycles = 4, exec = function(cpu) end },
   -- CMP Absolute,X
-  [0xDD] = function() end,
+  [0xDD] = { cycles = 4, exec = function(cpu) end },
   -- CMP Absolute,Y
-  [0xD9] = function() end,
+  [0xD9] = { cycles = 4, exec = function(cpu) end },
   -- CMP (Indirect,X)
-  [0xC1] = function() end,
+  [0xC1] = { cycles = 6, exec = function(cpu) end },
   -- CMP (Indirect),Y
-  [0xD1] = function() end,
+  [0xD1] = { cycles = 5, exec = function(cpu) end },
 
   -- CPX (Compare X Register)
   -- CPX Immediate
-  [0xE0] = function() end,
+  [0xE0] = { cycles = 2, exec = function(cpu) end },
   -- CPX Zero Page
-  [0xE4] = function() end,
+  [0xE4] = { cycles = 3, exec = function(cpu) end },
   -- CPX Absolute
-  [0xEC] = function() end,
+  [0xEC] = { cycles = 4, exec = function(cpu) end },
 
   -- CPY (Compare Y Register)
   -- CPY Immediate
-  [0xC0] = function() end,
+  [0xC0] = { cycles = 2, exec = function(cpu) end },
   -- CPY Zero Page
-  [0xC4] = function() end,
+  [0xC4] = { cycles = 3, exec = function(cpu) end },
   -- CPY Absolute
-  [0xCC] = function() end,
+  [0xCC] = { cycles = 4, exec = function(cpu) end },
 
   -- DEC (Decrement Memory)
   -- DEC Zero Page
-  [0xC6] = function() end,
+  [0xC6] = { cycles = 5, exec = function(cpu) end },
   -- DEC Zero Page,X
-  [0xD6] = function() end,
+  [0xD6] = { cycles = 6, exec = function(cpu) end },
   -- DEC Absolute
-  [0xCE] = function() end,
+  [0xCE] = { cycles = 6, exec = function(cpu) end },
   -- DEC Absolute,X
-  [0xDE] = function() end,
+  [0xDE] = { cycles = 7, exec = function(cpu) end },
 
   -- DEX (Decrement X Register)
   -- DEX Implied
-  [0xCA] = function() end,
+  [0xCA] = { cycles = 2, exec = function(cpu) end },
 
   -- DEY (Decrement Y Register)
   -- DEY Implied
-  [0x88] = function() end,
+  [0x88] = { cycles = 2, exec = function(cpu) end },
 
   -- EOR (Exclusive OR)
   -- EOR Immediate
-  [0x49] = function() end,
+  [0x49] = { cycles = 2, exec = function(cpu) end },
   -- EOR Zero Page
-  [0x45] = function() end,
+  [0x45] = { cycles = 3, exec = function(cpu) end },
   -- EOR Zero Page,X
-  [0x55] = function() end,
+  [0x55] = { cycles = 4, exec = function(cpu) end },
   -- EOR Absolute
-  [0x4D] = function() end,
+  [0x4D] = { cycles = 4, exec = function(cpu) end },
   -- EOR Absolute,X
-  [0x5D] = function() end,
+  [0x5D] = { cycles = 4, exec = function(cpu) end },
   -- EOR Absolute,Y
-  [0x59] = function() end,
+  [0x59] = { cycles = 4, exec = function(cpu) end },
   -- EOR (Indirect,X)
-  [0x41] = function() end,
+  [0x41] = { cycles = 6, exec = function(cpu) end },
   -- EOR (Indirect),Y
-  [0x51] = function() end,
+  [0x51] = { cycles = 5, exec = function(cpu) end },
 
   -- INC (Increment Memory)
   -- INC Zero Page
-  [0xE6] = function() end,
+  [0xE6] = { cycles = 5, exec = function(cpu) end },
   -- INC Zero Page,X
-  [0xF6] = function() end,
+  [0xF6] = { cycles = 6, exec = function(cpu) end },
   -- INC Absolute
-  [0xEE] = function() end,
+  [0xEE] = { cycles = 6, exec = function(cpu) end },
   -- INC Absolute,X
-  [0xFE] = function() end,
+  [0xFE] = { cycles = 7, exec = function(cpu) end },
 
   -- INX (Increment X Register)
   -- INX Implied
-  [0xE8] = function() end,
+  [0xE8] = { cycles = 2, exec = function(cpu) end },
 
   -- INY (Increment Y Register)
   -- INY Implied
-  [0xC8] = function() end,
+  [0xC8] = { cycles = 2, exec = function(cpu) end },
 
   -- JMP (Jump)
   -- JMP Absolute
-  [0x4C] = function() end,
+  [0x4C] = { cycles = 3, exec = function(cpu) end },
   -- JMP Indirect
-  [0x6C] = function() end,
+  [0x6C] = { cycles = 5, exec = function(cpu) end },
 
   -- JSR (Jump to Subroutine)
   -- JSR Absolute
-  [0x20] = function() end,
+  [0x20] = { cycles = 6, exec = function(cpu) end },
 
   -- LDA (Load Accumulator)
   -- LDA Immediate
-  [0xA9] = function() end,
+  [0xA9] = { cycles = 2, exec = function(cpu) end },
   -- LDA Zero Page
-  [0xA5] = function() end,
+  [0xA5] = { cycles = 3, exec = function(cpu) end },
   -- LDA Zero Page,X
-  [0xB5] = function() end,
+  [0xB5] = { cycles = 4, exec = function(cpu) end },
   -- LDA Absolute
-  [0xAD] = function() end,
+  [0xAD] = { cycles = 4, exec = function(cpu) end },
   -- LDA Absolute,X
-  [0xBD] = function() end,
+  [0xBD] = { cycles = 4, exec = function(cpu) end },
   -- LDA Absolute,Y
-  [0xB9] = function() end,
+  [0xB9] = { cycles = 4, exec = function(cpu) end },
   -- LDA (Indirect,X)
-  [0xA1] = function() end,
+  [0xA1] = { cycles = 6, exec = function(cpu) end },
   -- LDA (Indirect),Y
-  [0xB1] = function() end,
+  [0xB1] = { cycles = 5, exec = function(cpu) end },
 
   -- LDX (Load X Register)
   -- LDX Immediate
-  [0xA2] = function() end,
+  [0xA2] = { cycles = 2, exec = function(cpu) end },
   -- LDX Zero Page
-  [0xA6] = function() end,
+  [0xA6] = { cycles = 3, exec = function(cpu) end },
   -- LDX Zero Page,Y
-  [0xB6] = function() end,
+  [0xB6] = { cycles = 4, exec = function(cpu) end },
   -- LDX Absolute
-  [0xAE] = function() end,
+  [0xAE] = { cycles = 4, exec = function(cpu) end },
   -- LDX Absolute,Y
-  [0xBE] = function() end,
+  [0xBE] = { cycles = 4, exec = function(cpu) end },
 
   -- LDY (Load Y Register)
   -- LDY Immediate
-  [0xA0] = function() end,
+  [0xA0] = { cycles = 2, exec = function(cpu) end },
   -- LDY Zero Page
-  [0xA4] = function() end,
+  [0xA4] = { cycles = 3, exec = function(cpu) end },
   -- LDY Zero Page,X
-  [0xB4] = function() end,
+  [0xB4] = { cycles = 4, exec = function(cpu) end },
   -- LDY Absolute
-  [0xAC] = function() end,
+  [0xAC] = { cycles = 4, exec = function(cpu) end },
   -- LDY Absolute,X
-  [0xBC] = function() end,
+  [0xBC] = { cycles = 4, exec = function(cpu) end },
 
   -- LSR (Logical Shift Right)
   -- LSR Accumulator
-  [0x4A] = function() end,
+  [0x4A] = { cycles = 2, exec = function(cpu) end },
   -- LSR Zero Page
-  [0x46] = function() end,
+  [0x46] = { cycles = 5, exec = function(cpu) end },
   -- LSR Zero Page,X
-  [0x56] = function() end,
+  [0x56] = { cycles = 6, exec = function(cpu) end },
   -- LSR Absolute
-  [0x4E] = function() end,
+  [0x4E] = { cycles = 6, exec = function(cpu) end },
   -- LSR Absolute,X
-  [0x5E] = function() end,
+  [0x5E] = { cycles = 7, exec = function(cpu) end },
 
   -- NOP (No Operation)
   -- NOP Implied
-  [0xEA] = function() end,
+  [0xEA] = { cycles = 2, exec = function(cpu) end },
 
   -- ORA (Logical Inclusive OR)
   -- ORA Immediate
-  [0x09] = function() end,
+  [0x09] = { cycles = 2, exec = function(cpu) end },
   -- ORA Zero Page
-  [0x05] = function() end,
+  [0x05] = { cycles = 3, exec = function(cpu) end },
   -- ORA Zero Page,X
-  [0x15] = function() end,
+  [0x15] = { cycles = 4, exec = function(cpu) end },
   -- ORA Absolute
-  [0x0D] = function() end,
+  [0x0D] = { cycles = 4, exec = function(cpu) end },
   -- ORA Absolute,X
-  [0x1D] = function() end,
+  [0x1D] = { cycles = 4, exec = function(cpu) end },
   -- ORA Absolute,Y
-  [0x19] = function() end,
+  [0x19] = { cycles = 4, exec = function(cpu) end },
   -- ORA (Indirect,X)
-  [0x01] = function() end,
+  [0x01] = { cycles = 6, exec = function(cpu) end },
   -- ORA (Indirect),Y
-  [0x11] = function() end,
+  [0x11] = { cycles = 5, exec = function(cpu) end },
 
   -- PHA (Push Accumulator)
   -- PHA Implied
-  [0x48] = function() end,
+  [0x48] = { cycles = 3, exec = function(cpu) end },
 
   -- PHP (Push Processor Status)
   -- PHP Implied
-  [0x08] = function() end,
+  [0x08] = { cycles = 3, exec = function(cpu) end },
 
   -- PLA (Pull Accumulator)
   -- PLA Implied
-  [0x68] = function() end,
+  [0x68] = { cycles = 4, exec = function(cpu) end },
 
   -- PLP (Pull Processor Status)
   -- PLP Implied
-  [0x28] = function() end,
+  [0x28] = { cycles = 4, exec = function(cpu) end },
 
   -- ROL (Rotate Left)
   -- ROL Accumulator
-  [0x2A] = function() end,
+  [0x2A] = { cycles = 2, exec = function(cpu) end },
   -- ROL Zero Page
-  [0x26] = function() end,
+  [0x26] = { cycles = 5, exec = function(cpu) end },
   -- ROL Zero Page,X
-  [0x36] = function() end,
+  [0x36] = { cycles = 6, exec = function(cpu) end },
   -- ROL Absolute
-  [0x2E] = function() end,
+  [0x2E] = { cycles = 6, exec = function(cpu) end },
   -- ROL Absolute,X
-  [0x3E] = function() end,
+  [0x3E] = { cycles = 7, exec = function(cpu) end },
 
   -- ROR (Rotate Right)
   -- ROR Accumulator
-  [0x6A] = function() end,
+  [0x6A] = { cycles = 2, exec = function(cpu) end },
   -- ROR Zero Page
-  [0x66] = function() end,
+  [0x66] = { cycles = 5, exec = function(cpu) end },
   -- ROR Zero Page,X
-  [0x76] = function() end,
+  [0x76] = { cycles = 6, exec = function(cpu) end },
   -- ROR Absolute
-  [0x6E] = function() end,
+  [0x6E] = { cycles = 6, exec = function(cpu) end },
   -- ROR Absolute,X
-  [0x7E] = function() end,
+  [0x7E] = { cycles = 7, exec = function(cpu) end },
 
   -- RTI (Return from Interrupt)
   -- RTI Implied
-  [0x40] = function() end,
+  [0x40] = { cycles = 6, exec = function(cpu) end },
 
   -- RTS (Return from Subroutine)
   -- RTS Implied
-  [0x60] = function() end,
+  [0x60] = { cycles = 6, exec = function(cpu) end },
 
   -- SBC (Subtract with Carry)
   -- SBC Immediate
-  [0xE9] = function() end,
+  [0xE9] = { cycles = 2, exec = function(cpu) end },
   -- SBC Zero Page
-  [0xE5] = function() end,
+  [0xE5] = { cycles = 3, exec = function(cpu) end },
   -- SBC Zero Page,X
-  [0xF5] = function() end,
+  [0xF5] = { cycles = 4, exec = function(cpu) end },
   -- SBC Absolute
-  [0xED] = function() end,
+  [0xED] = { cycles = 4, exec = function(cpu) end },
   -- SBC Absolute,X
-  [0xFD] = function() end,
+  [0xFD] = { cycles = 4, exec = function(cpu) end },
   -- SBC Absolute,Y
-  [0xF9] = function() end,
+  [0xF9] = { cycles = 4, exec = function(cpu) end },
   -- SBC (Indirect,X)
-  [0xE1] = function() end,
+  [0xE1] = { cycles = 6, exec = function(cpu) end },
   -- SBC (Indirect),Y
-  [0xF1] = function() end,
+  [0xF1] = { cycles = 5, exec = function(cpu) end },
 
   -- Set Carry, Decimal and Interrupt flags
   -- SEC Implied
-  [0x38] = function() end,
+  [0x38] = { cycles = 2, exec = function(cpu) end },
   -- SED Implied
-  [0xF8] = function() end,
+  [0xF8] = { cycles = 2, exec = function(cpu) end },
   -- SEI Implied
-  [0x78] = function() end,
+  [0x78] = { cycles = 2, exec = function(cpu) end },
 
   -- STA (Store Accumulator)
   -- STA Zero Page
-  [0x85] = function() end,
+  [0x85] = { cycles = 3, exec = function(cpu) end },
   -- STA Zero Page,X
-  [0x95] = function() end,
+  [0x95] = { cycles = 4, exec = function(cpu) end },
   -- STA Absolute
-  [0x8D] = function() end,
+  [0x8D] = { cycles = 4, exec = function(cpu) end },
   -- STA Absolute,X
-  [0x9D] = function() end,
+  [0x9D] = { cycles = 5, exec = function(cpu) end },
   -- STA Absolute,Y
-  [0x99] = function() end,
+  [0x99] = { cycles = 5, exec = function(cpu) end },
   -- STA (Indirect,X)
-  [0x81] = function() end,
+  [0x81] = { cycles = 6, exec = function(cpu) end },
   -- STA (Indirect),Y
-  [0x91] = function() end,
+  [0x91] = { cycles = 6, exec = function(cpu) end },
 
   -- STX (Store X Register)
   -- STX Zero Page
-  [0x86] = function() end,
+  [0x86] = { cycles = 3, exec = function(cpu) end },
   -- STX Zero Page,Y
-  [0x96] = function() end,
+  [0x96] = { cycles = 4, exec = function(cpu) end },
   -- STX Absolute
-  [0x8E] = function() end,
+  [0x8E] = { cycles = 4, exec = function(cpu) end },
 
   -- STY (Store Y Register)
   -- STY Zero Page
-  [0x84] = function() end,
+  [0x84] = { cycles = 3, exec = function(cpu) end },
   -- STY Zero Page,X
-  [0x94] = function() end,
+  [0x94] = { cycles = 4, exec = function(cpu) end },
   -- STY Absolute
-  [0x8C] = function() end,
+  [0x8C] = { cycles = 4, exec = function(cpu) end },
 }
 
 local MOS6502 = {}
@@ -422,6 +422,17 @@ function MOS6502:step()
   local operation = opcodes[opcode]
   if operation then
     operation(self)
+  end
+end
+
+function MOS6502:step()
+  local opcode = self:fetch()
+  local instruction = opcodes[opcode]
+  if instruction then
+    instruction.exec(self)
+    self.cycles = self.cycles + instruction.cycles
+  else
+    error(string.format("opcode 0x%02X not implemented.", opcode))
   end
 end
 
